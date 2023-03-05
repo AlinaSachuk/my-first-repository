50f6187c994d53dae0f75341964fad320c8aa3f7
AlinaSachuk <alinko-91@yandex.ru>
2023-03-05 20:06:40 +0300

    Start BeautyProjectApplication

---
+++ src/test/java/com/example/beauty_project/BeautyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beauty_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
